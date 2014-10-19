bb9d0f0b885aca0617626182e15383d6981a9d72
billy wintergreen <selig2@illinois.edu>
2014-10-18 20:18:16 -0500

    calculated r for all stats

--- footballPredictor/getAllData.py
+++ footballPredictor/getAllData.py
@@ -1,5 +1,7 @@
 import csv
+from statisticalFunctions import *
 def getAllData():
+
 	gamesWonFile = open('/home/lucas/github/footballData/csvStats/gamesWon.csv') #get the gamesWon data. Nicely formatted.
 	gamesWon = csv.reader(gamesWonFile)
 	gamesWonArray = [] # put it into a 2-d array
@@ -65,6 +67,7 @@ def getAllData():
 	thirdDownPercentArray = []
 	for row in thirdDownPercent:
 		thirdDownPercentArray.append(row)
+
 #######################################################################
 # formatting the data so that it can be used (removing '%' and '+')
 
@@ -75,7 +78,68 @@ def getAllData():
 
 			redZoneTDArray[i][j] = redZoneTDArray[i][j][:-1] #get rid of %
 			thirdDownPercentArray[i][j] = thirdDownPercentArray[i][j][:-1] #get rid of %
+
+	passerRatingDiff = []
+	for i in range(1, 33):
+		for j in range(2, 14):
+			atprArray[i][j] = float(atprArray[i][j]) - float(oatprArray[i][j])
+	passerRatingDiff = atprArray
+
+########################################################################
+	yppR = []
+	turnoverMarginR = []
+	takeawaysR = []
+	redZoneTDPercentR = []
+	pppMarginR = []
+	pppR = []
+	fieldGoalsR = []
+	thirdDownPercentR = []
+	passerRatingDiffR = []
+	#for i in range(1, 33):
+	for j in range(2, 13): #get the correlation coefficent between ____ and gamesWon for each season since 2003
+		yppR.append(findR(yppArray, gamesWonArray, 32, j))
+		turnoverMarginR.append(findR(turnoverMarginArray, gamesWonArray, 32, j))
+		redZoneTDPercentR.append(findR(redZoneTDArray, gamesWonArray, 32, j))
+		pppMarginR.append(findR(pppMarginArray, gamesWonArray, 32, j))
+		pppR.append(findR(pppArray, gamesWonArray, 32, j))
+		fieldGoalsR.append(findR(fieldGoalsArray, gamesWonArray, 32, j))
+		thirdDownPercentR.append(findR(thirdDownPercentArray, gamesWonArray, 32, j))
+		passerRatingDiffR.append(findR(passerRatingDiff, gamesWonArray, 32, j))
+
 	
+	yearMap = {0: '2003', 1: '2004', 2: '2005', 3: '2006', 4: '2007', 5: '2008', 6: '2009', 7: '2010', 8: '2011', 9: '2012', 10: '2013'}
+	print "##############################################################\n\n"
+	print "Max of yppR: " + str(max(yppR)) + " in year: " + yearMap[yppR.index(max(yppR))]
+	print "Max of turnoverMarginR: " + str(max(turnoverMarginR)) + " in year: " + yearMap[turnoverMarginR.index(max(turnoverMarginR))]
+	print "Max of redZoneTDPercentR: " + str(max(redZoneTDPercentR)) + " in year: " + yearMap[redZoneTDPercentR.index(max(redZoneTDPercentR))]
+	print "Max of pppMarginR: " + str(max(pppMarginR)) + " in year: " + yearMap[pppMarginR.index(max(pppMarginR))]
+	print "Max of pppR: " + str(max(pppR)) + " in year: " + yearMap[pppR.index(max(pppR))]
+	print "Max of fieldGoalsR: " + str(max(fieldGoalsR)) + " in year: " + yearMap[fieldGoalsR.index(max(fieldGoalsR))]
+	print "Max of thirdDownPercentR: " + str(max(thirdDownPercentR)) + " in year: " + yearMap[thirdDownPercentR.index(max(thirdDownPercentR))]
+	print "Max of passerRatingDiffR: " + str(max(passerRatingDiffR)) + " in year: " + yearMap[passerRatingDiffR.index(max(passerRatingDiffR))]
+	print "\n\n##############################################################"
+
+
+	print "min of yppR: " + str(min(yppR)) + " in year: " + yearMap[yppR.index(min(yppR))]
+	print "min of turnoverMarginR: " + str(min(turnoverMarginR)) + " in year: " + yearMap[turnoverMarginR.index(min(turnoverMarginR))]
+	print "min of redZoneTDPercentR: " + str(min(redZoneTDPercentR)) + " in year: " + yearMap[redZoneTDPercentR.index(min(redZoneTDPercentR))]
+	print "min of pppMarginR: " + str(min(pppMarginR)) + " in year: " + yearMap[pppMarginR.index(min(pppMarginR))]
+	print "min of pppR: " + str(min(pppR)) + " in year: " + yearMap[pppR.index(min(pppR))]
+	print "min of fieldGoalsR: " + str(min(fieldGoalsR)) + " in year: " + yearMap[fieldGoalsR.index(min(fieldGoalsR))]
+	print "min of thirdDownPercentR: " + str(min(thirdDownPercentR)) + " in year: " + yearMap[thirdDownPercentR.index(min(thirdDownPercentR))]
+	print "min of passerRatingDiffR: " + str(min(passerRatingDiffR)) + " in year: " + yearMap[passerRatingDiffR.index(min(passerRatingDiffR))]
+	print "\n\n##############################################################"
+
+
+	yppR = averageOfArray(yppR) #get the average of each R for each stat vs gamesWon since 2003
+	turnoverMarginR = averageOfArray(turnoverMarginR)
+	redZoneTDPercentR = averageOfArray(redZoneTDPercentR)
+	pppMarginR = averageOfArray(pppMarginR)
+	pppR = averageOfArray(pppR)
+	fieldGoalsR = averageOfArray(fieldGoalsR)
+	thirdDownPercentR = averageOfArray(thirdDownPercentR)
+	passerRatingDiffR = averageOfArray(passerRatingDiffR)
+
 
 
 
---
+++ footballPredictor/statisticalFunctions.py
@@ -0,0 +1,52 @@
+def sumRow(target, row): # where target is a double array
+
+	sum = 0
+	for j in range(2, 13): # go from 2003 to 2013, ignore 2014.6 for now
+		sum += float(target[row][j])
+	return sum
+
+def sumColumn(target, column):
+	sum = 0
+	for j in range(2, 33):
+		sum += float(target[j][column])
+	return sum
+
+def sumSquaresColumn(target, column):
+	sum = 0
+	for j in range(2, 33):
+		sum += (float(target[j][column]) ** 2)
+	return sum
+
+def sumXYColumn(target1, target2, column):
+	sum = 0
+	for j in range(2, 33):
+		sum += (float(target1[j][column]) * float(target2[j][column]))
+	return sum
+
+def averageOfArray(target):
+	length = len(target)
+	sum = 0
+	for i in range(length):
+		sum+=float(target[i])
+
+	return (sum / length)
+
+
+
+def findR(stat1Array, stat2Array, n, column): #eg find the correlation between column 2 (year 2003) of YPP vs GamesWon. n should be the number of pairs of data. Aka 32 for our purposes.
+	sumX = sumColumn(stat1Array, column)# step 1: sum up all 'x' coordinates
+	sumY = sumColumn(stat2Array, column) # step 2: sum up all'y' coordinates
+	sumXSquared = sumSquaresColumn(stat1Array, column) # sum up all 'x^2' coordinates
+	sumYSquared = sumSquaresColumn(stat2Array, column) # sum up all 'y^2' coordinates
+	sumXY = sumXYColumn(stat1Array, stat2Array, column)
+
+
+	r = ((n * sumXY) - (sumX * sumY)) / ((((n * sumXSquared) - (sumX ** 2)) ** .5) * (((n * sumYSquared) - (sumY ** 2)) ** .5))
+	return r 
+
+
+
+
+
+
+
